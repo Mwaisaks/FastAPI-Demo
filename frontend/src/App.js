6488bc4c8c8aa512343005e437ea5a255656f401
Mwaisaks <mwaisakaphenny@gmail.com>
2025-11-04 14:52:28 +0300

    fixed frontend deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: "fastapi-demo-backend.up.railway.app",
 });
 
 function App() {
